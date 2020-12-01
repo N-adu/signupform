69206edf19cfffccbd394984a9cdcd28c6b5acf9
N Adu <64503636+N-adu@users.noreply.github.com>
2020-12-01 23:46:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+
+function showError(input, message) {
+    const formControl = input.parentElement;    
+    formControl.className = 'form-control error';  
+    const small = formControl.querySelector('small') 
+    small.innerText = message; 
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;   
+    formControl.className = 'form-control success';  
+}
+
+function checkEmail(input){
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ //   return re.test(email);
+ if(re.test(input.value.trim())) {
+     showSuccess(input);
+ } else {
+    showError(input, 'Email is not valid!') 
+ }
+ };
+
+ function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    }); 
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1); 
+};
+
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+ }  
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkRequired([username, email, password]);  
+    checkLength(username, 5, 10);
+    checkLength(password, 6, 15);
+    checkEmail(email);
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
